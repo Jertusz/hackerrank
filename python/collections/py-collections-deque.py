3431f4c1b4423d913950940d6cbb9234ccce740e
Jedrzej Szadejko <jendziu@gmail.com>
2020-09-10 20:36:23 +0200

    collections finished

---
+++ python/collections/py-collections-deque.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+d = deque()
+for _ in range(int(input())):
+    command = input().split()
+    if command[0] == 'append':
+        d.append(int(command[1]))
+    elif command[0] == 'appendleft':
+        d.appendleft(int(command[1]))
+    elif command[0] == 'pop':
+        d.pop()
+    elif command[0] == 'popleft':
+        d.popleft()
+print(*d)

19b1e1e84b145de65acef29028926e273d430997
Jedrzej Szadejko <jendziu@gmail.com>
2020-09-10 20:10:05 +0200

    added collections/defaultdict-tutoral exercise

---
+++ python/collections/defaultdict-tutorial.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+n, m = map(int, input().split(' '))
+input1 = list()
+
+for i in range(n):
+    input1.append(input())
+
+input2 = list()
+for i in range(m):
+    input2.append(input())
+
+d = defaultdict(list)
+
+for i in range(n):
+    d[input1[i]].append(i+1)
+for i in input2:    
+    if i in d:
+        print(*d[i])
+    else:
+        print(-1)
